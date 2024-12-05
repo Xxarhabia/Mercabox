9ca00a443e005eec56d8a89a8e18ece4d435bae7
MarioSarabia <mariosarabia@wposs.com>
2024-12-05 17:17:41 -0500

    ➕ Agregamos el login del usuario

--- MercaBox-Microservices/microservice-user/src/main/java/com/msara/controller/dto/response/AuthResponse.java
+++
@@ -1,7 +0,0 @@
-package com.msara.controller.dto.response;
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-@JsonPropertyOrder({"username", "message, auth_token, verification_code"})
-public record AuthResponse(String username, String message, String authToken, String verificationCode){
-}
